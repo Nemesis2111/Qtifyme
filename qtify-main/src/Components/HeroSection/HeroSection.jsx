783a4372215883b56f701d78c934d1ade0d82620
Aniket <aniketchatterjee700@gmail.com>
2024-09-27 03:09:53 +0530

    My Qtify2

--- qtify-main/src/Components/HeroSection/HeroSection.jsx
+++ qtify-main/src/Components/HeroSection/HeroSection.jsx
@@ -2,6 +2,7 @@ import './HeroSection.css';
 const HeroSection = () => {
   return (
     <div className="hero-section">
+       <h1>100 Thousand Songs, ad-free</h1>
       <img
         src="HeroImage.png"
         alt="100 Thousand Songs, ad-free Over thousands podcast episodes"
